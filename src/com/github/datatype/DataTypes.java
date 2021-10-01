4425395847b6880e66f9d477ace22d396e3a6767
Gamer <Gamer@Gamer>
2021-10-01 01:03:48 -0500

    Data types byte, char, long and double are included

--- src/com/github/datatype/DataTypes.java
+++ src/com/github/datatype/DataTypes.java
@@ -15,7 +15,20 @@ public class DataTypes {
 		
 		short M = -32500;
 		System.out.println("Short Value is " + M);
-
+		
+		//Anas worked on byte, char, long and double
+		
+		byte b = 2;
+		System.out.println("Byte value is " + b);
+		
+		char c = 'P';
+		System.out.println("Char value is " + c);
+		
+		long l = 789123;
+		System.out.println("Long value is " + l);
+		
+		double d = 0.0896;
+		System.out.println("Double value is " + d);
 	}
 
 }
