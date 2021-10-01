40d94d3703941173332fd564e3fd3a8f49512d41
18456 <18456@LAPTOP-957DPV2H>
2021-10-01 00:44:52 -0500

    Data types int, boolean, float and short.

---
+++ src/com/github/datatype/DataTypes.java
@@ -0,0 +1,21 @@
+package com.github.datatype;
+
+public class DataTypes {
+	
+	public static void main(String[] args) {
+		
+		int n = 1;
+		System.out.println("Int value is " + n);
+		
+		boolean Ma = false;
+		System.out.println("boolean value is " + Ma);
+		
+		float X = 0.01f;
+		System.out.println("Float value is " + X);
+		
+		short M = -32500;
+		System.out.println("Short Value is " + M);
+
+	}
+
+}
